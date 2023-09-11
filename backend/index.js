66f96e651db1f9a5097411e89e490928e2646092
ProfBatsi <bmmugomba@gmail.com>
2023-09-11 14:24:47 +0200

    route update books

--- backend/index.js
+++ backend/index.js
@@ -45,8 +45,55 @@ const newBook = {
 app.get('/books', async(request, response) => {
     try{
     const books = await Book.find({});
-    return response.status(200).json(books);
+    return response.status(200).json({
+        count: books.length,
+        data: books
+
+    });
     }catch(error) {
+        console.log(error.message);
+        response.status(500).send({message : error.message});
+    }
+})
+
+//route to getting one book by database id
+app.get('/books/:id', async(request, response) => {
+    try{
+
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+    }catch(error) {
+        console.log(error.message);
+        response.status(500).send({message : error.message});
+    }
+})
+
+//request for book update
+app.put('/books/:id', async(request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) { 
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id,request.body);
+
+        if(!result) {
+            return response.status(404).json({message: 'book not found'})
+        }
+
+        return response.status(200).send({message: 'Book updated successfully'})
+
+    } catch(error) {
         console.log(error.message);
         response.status(500).send({message : error.message});
     }
