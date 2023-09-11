d12c7c1d44ac97d41752b174c619ccfbe9ee4cbf
ProfBatsi <bmmugomba@gmail.com>
2023-09-11 12:39:21 +0200

    backednd init commit

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,6 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.wpyafgl.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+//please create a free database for yourself
+//This database will be deleted after  tutorial
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,49 @@
-import express from "express";
-import {PORT} from "./config.js";
+import express, { request, response } from "express";
+import {PORT,mongoDBURL} from "./config.js";
+
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(PORT, () =>{
-    console.log(`App is listening to port: ${PORT}`);
-})
+//middlware for passing request body
+
+app.get('/', (request,response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack Tutorial');
+});
+
+// Route for save a new book
+app.post('/books',async(request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) { 
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database'); 
+        app.listen(PORT, () =>{
+            console.log(`App is listening to port: ${PORT}`);
+        });
+        
+    })
+    .catch(() => {
+
+    })
